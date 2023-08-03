c8e4a15c629f156cdcca32ba1174f5d688c69ef3
Eduardo Borges <euduardoborges@gmail.com>
2023-08-03 11:10:42 -0300

    chore(examples): update examples

--- examples/vite-project/src/App.tsx
+++ examples/vite-project/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
 import { Form, Field } from 'react-final-form';
-import { useHookFormMask, withMask } from '../../../src';
+import { useHookFormMask, withMask } from 'use-mask-input';
 import "./App.css";
 
 function App() {
