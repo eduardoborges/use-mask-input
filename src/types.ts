2e17b794ec2290dde1a911e632ee0090663602a7
Lucas Travessa <121526431+LucasTravessa@users.noreply.github.com>
2024-07-23 10:37:08 -0300

    feat: update mask type to match the avaliable types. (#88)

--- src/types.ts
+++ src/types.ts
@@ -2,6 +2,6 @@ import { Options as MaskOptions } from './inputmask.types';
 
 export type { UseFormRegister, UseFormRegisterReturn } from 'react-hook-form';
 
-export type Mask = 'email' | 'cpf' | 'datetime' | 'numeric' | 'currency' | 'decimal' | 'integer' | (string & {}) | (string[] & {}) | null;
+export type Mask = 'datetime' | 'email' | 'numeric' | 'currency' | 'decimal' | 'integer' | 'percentage' | 'url' | 'ip' | 'mac' | 'ssn' | 'brl-currency' | 'cpf' | 'cnpj' | (string & {}) | (string[] & {}) | null;
 export type Options = MaskOptions;
 export type Input = HTMLInputElement | HTMLTextAreaElement | HTMLElement | HTMLInputElement | null;
