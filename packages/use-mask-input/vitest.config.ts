1448ee8a0b91f2b057de989a6581818eac589949
Eduardo Borges <euduardoborges@gmail.com>
2026-05-22 11:06:34 -0300

    fix: Codecov integration and enhance coverage reporting

--- packages/use-mask-input/vitest.config.ts
+++ packages/use-mask-input/vitest.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
     },
     coverage: {
       provider: 'v8',
+      reporter: ['text', 'lcov', 'json'],
       include: ['src/**/*.{ts,tsx}'],
       exclude: [
         '/**/index.{ts,tsx}',
